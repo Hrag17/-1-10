1cefc14428fad16384028726b26b503235f27398
Hrag17 <58171813+Hrag17@users.noreply.github.com>
2020-12-18 14:32:38 +0400

    Add files via upload

---
+++ 1to10.js
@@ -0,0 +1,47 @@
+var a=prompt("Գուշակիր թիվը 0-10");
+switch(a)
+{
+case"0":
+ a="Սխալ";
+     break;
+ case"1":
+ a="Սխալ";
+     break;
+ case"2":
+ a="Սխալ";
+     break;
+ case"3":
+ a="Սխալ";
+     break;
+ case"4":
+ a="ճիշդ";
+     break;
+ case"5":
+ a="սխալ";
+  break;
+ case"6":
+ a="սխալ";
+  break;
+ case"7":
+ a="սխալ"; 
+   break;
+   case"8":
+ a="սխալ"; 
+   break;
+   case"9":
+ a="սխալ"; 
+   break;
+   case"10":
+ a="սխալ"; 
+   break;
+   
+ 
+  default:
+    a="10-ից բարձր չի կարելի";
+
+}
+document.write(a);
+
+
+
+
